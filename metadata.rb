fa3e7381bfec2c802178051245ce09c3f5db3248
Rohan Kapadia <rohankapadia@gmail.com>
2014-02-13 16:55:42 +0530

    initial commit.

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'storm-vagrant'
+maintainer       'Rohan Kapadia'
+maintainer_email 'rohankapadia@gmail.com'
+license          'Apache2'
+description      'Installs/Configures storm-vagrant'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "apt"
+depends "java"
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: storm-vagrant
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
