2564375b24c480b6d10db1e8e4e7d25f3a08347d
Rohan Kapadia <rohankapadia@gmail.com>
2014-02-18 13:55:22 +0530

    updated attributes to override ones.

--- attributes/java.rb
+++ attributes/java.rb
@@ -1,3 +1,3 @@
-default['java']['jdk_version'] = '7'
-default['java']['install_flavor'] = 'oracle'
+override['java']['jdk_version'] = '7'
+override['java']['install_flavor'] = 'oracle'
 override['java']['oracle']['accept_oracle_download_terms'] = true
