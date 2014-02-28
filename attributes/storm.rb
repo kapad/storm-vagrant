3f0bb0fb67e5a749878d3d396236b0d1bb48b685
Rohan Kapadia <rohankapadia@gmail.com>
2014-02-28 16:48:44 +0530

    Added adittionally required attributes.

--- attributes/storm.rb
+++ attributes/storm.rb
@@ -1,2 +1,4 @@
 default['storm']['zookeeper']['cluster_name'] = 'vagrant-solo'
 default['storm']['topology']['debug'] = true
+default['storm']['cluster_name'] = "vagrant-solo-box"
+default['storm']['drpc']['switch'] = true
