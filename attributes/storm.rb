7367df39ff261ab565334b07cd8aef3f70736c36
Rohan Kapadia <rohankapadia@gmail.com>
2014-03-10 20:50:43 +0530

    added setting to override debug=false for runit logs.

--- attributes/storm.rb
+++ attributes/storm.rb
@@ -2,3 +2,4 @@ default['storm']['zookeeper']['cluster_name'] = 'vagrant-solo'
 default['storm']['topology']['debug'] = true
 default['storm']['cluster_name'] = "vagrant-solo-box"
 default['storm']['drpc']['switch'] = true
+default['storm']['runit']['verbose'] = true
